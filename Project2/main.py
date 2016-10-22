dc09ddb0ebba9fd69a8eb043271c88a81ef22960
Zackery Griesinger <zgriesinger@gmail.com>
2016-10-22 13:04:18 -0500

    Did some more testing with main.py, found a solution and detailed it in algorithm.txt

--- Project2/main.py
+++ Project2/main.py
@@ -1,5 +1,4 @@
 import math
-
 def get_isbn_index(ISBN):
 	for i in books:
 		if(i[0] == ISBN):
@@ -18,11 +17,30 @@ class Customer:
 		#print(self.id_num + " " + self.name + " " + ','.join(self.reviews))
 		print(str(self.id_num))
 
+def cs(v1, v2):
+	"""Compute cosine similarity between to vectors"""
+	sumxx, sumxy, sumyy = 0, 0, 0
+	for i in range(len(v1)):
+		x = v1[i]; y = v2[i]
+		sumxx += x*x
+		sumyy += y*y
+		sumxy += x*y
+	return sumxy/math.sqrt(sumxx*sumyy)
+def rd(lst):
+	lst_sum = 0
+	counter = 0
+	for i in lst:
+		if i != 0:
+			lst_sum += i
+			counter += 1
+	avg = lst_sum / counter
+	for i in range(len(lst)):
+		if lst[i] != 0:
+			lst[i] = lst[i] - avg
+	return lst
 
-
-
-input_ratings = open("ratings1.txt")
-
+def find_most_similar(lst):
+	return lst.index(max(lst))
 books = []
 with open("books1.txt") as f:
 	for i in f:
@@ -45,7 +63,7 @@ with open("customers1.txt") as g:
 	#for i in range(len(books)):
 		#print(customer.reviews[i])
 
-with open("ratings.txt") as h:
+with open("ratings1.txt") as h:
 	for k in h:
 		line = k.split(",")
 		#print(line)
@@ -59,15 +77,36 @@ with open("ratings.txt") as h:
 			for customer in customers:
 				if(id_num == int(customer.id_num)):
 					customer.set_rating(isbn, rating)
+utility_matrix = []
+col = []
+user_similaritys = []
+all_users = []
 
-for customer in customers:
-	print(str(customer.id_num) + " " + customer.name)
-	print(customer.reviews)
+for i in range(len(customers)):
+	for j in range(len(customers)):
+	#print(str(customer.id_num) + " " + customer.name)
+		if i != j:
+			user_similaritys.append(cs(rd(customers[i].reviews), rd(customers[j].reviews)))
+			print(customers[i].name + " vs. " + customers[j].name, end=" ")
+			print(cs(rd(customers[i].reviews), rd(customers[j].reviews)))
+	all_users.append(user_similaritys)
+	user_similaritys = []
 
 
+for i in all_users:
+	print(i)
+
+print(find_most_similar(all_users[0]))
+
+
+# for i in range(len(utility_matrix[0])):
+#	for j in range(len(utility_matrix)):
+#		col.append(utility_matrix[j][i])
+
+#print(cosine_similarity(reduce_disparity(utility_matrix[0]), reduce_disparity(utility_matrix[1])))
+	
 #review_list = ""
 # for customer in customers:
 # 	print(str(customer.id_num + " " + customer.name))
 # 	for i in range(len(books)):
 # 		review_list = review_list + str(customer.reviews[i])
-
